2096ebee6a4a467fa2310d21bb4725601cc4e904
Christian Chaux <christian.chaux@bionomeex.com>
2021-10-18 15:40:44 +0200

    Fix metadata implementation

--- Luciol.Diagnostics/Plugin.cs
+++ Luciol.Diagnostics/Plugin.cs
@@ -1,13 +1,7 @@
 ﻿using Avalonia.Controls;
 using Luciol.Plugin;
-using Luciol.Plugin.AssemblyAttribute;
 using Luciol.Plugin.Preference;
 using System.Collections.Generic;
-using System.Reflection;
-
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyAuthor("BionomeeX")]
-[assembly: AssemblyDescription("Diagnostic tools")]
 
 namespace Luciol.Diagnostics
 {
---
+++ Luciol.Diagnostics/PluginInfo.cs
@@ -0,0 +1,20 @@
+﻿using Luciol.Plugin;
+using System;
+
+namespace Luciol.Diagnostics
+{
+    public class PluginInfo : APluginInfo
+    {
+        public override string Description => "Diagnostic tools";
+
+        public override string Version => "1.0.0";
+
+        public override PluginType PluginType => PluginType.Display;
+
+        protected override string Name => "Diagnostics";
+
+        protected override string Author => "BionomeeX";
+
+        protected override Type Plugin => typeof(Plugin);
+    }
+}
